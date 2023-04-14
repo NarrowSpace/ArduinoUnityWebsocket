83d5407e5b287f76ed31d5a76402dc1bfba8ad58
NarrowSpace <zhuwentianzz@gmail.com>
2023-04-14 00:30:41 -0400

    Add empty project

--- Robot Gamebo/Assets/WebSocketClient.cs
+++ Robot Gamebo/Assets/WebSocketClient.cs
@@ -1,11 +1,31 @@
 using WebSocketSharp;
 using UnityEngine;
+using UnityEngine.UI;
 using System;
 using UnityEngine.SocialPlatforms.Impl;
+
+
+// UnityMainThreadDispatcher library: https://github.com/PimDeWitte/UnityMainThreadDispatcher
+// WebSocketSharp Library: https://github.com/sta/websocket-sharp
 
 public class WebSocketClient : MonoBehaviour
 {
     WebSocket ws;
+    private int myScore = 0;
+    private int prevScore = -1; // set to -1 initially to ensure animation triggers on first score update
+    public Text myScoreText;
+    public Animator manAnimator;
+    private bool isAnimationTriggered = false;
+    public Animator monsterAnimator;
+
+    public AudioSource src;
+    public AudioClip Hurt;
+
+    private void Awake()
+    {
+        manAnimator = GameObject.Find("GunMan").GetComponent<Animator>();
+        monsterAnimator = GameObject.Find("Monster").GetComponent<Animator>();
+    }
 
     void Start()
     {
@@ -14,25 +34,79 @@ public class WebSocketClient : MonoBehaviour
         ws.OnMessage += (sender, e) =>
         {
             Debug.Log("Message Received from " + ((WebSocket)sender).Url + ", Data : " + e.Data);
+            UpdateScore(e.Data);
         };
 
         ws.Connect();
-        ws.Send("Hello from Unity");
     }
 
-
-    private void Update()
+    private void UpdateScore(string jsonData)
     {
-        if (ws == null)
+        // Use the Try and Catch function to avoid situations where there may be JSON data that cannot be processed or issues with conversion.
+        try
         {
-            return;
+            // Converts a JSON string into an object of the specified type
+            ScoreData scoreData = JsonUtility.FromJson<ScoreData>(jsonData);
+            myScore = scoreData.Counter;
+            Debug.Log("Score: " + myScore);
+
+            // Update in the Main Thread
+            UnityMainThreadDispatcher.Instance().Enqueue(() =>
+            {
+                myScoreText.text = "Score: " + myScore.ToString();
+
+                if (myScore != prevScore) //Trigger GunMan Animation
+                {
+                    if (prevScore < myScore && prevScore != -1 && (myScore - prevScore) == 1)
+                    {
+                        manAnimator.SetTrigger("Shoot");
+
+                        monsterAnimator.SetTrigger("GetHit");
+
+                       src.clip = Hurt;
+
+                        src.Play();
+
+                        isAnimationTriggered = true;
+
+                        Debug.Log("Shoot!");
+
+                    }
+
+                    else if (prevScore > myScore || prevScore == myScore || prevScore == 0)
+                    {
+                        isAnimationTriggered = false;
+                    }
+                }
+
+               prevScore = myScore; //Update the score
+
+            });
         }
 
-        if(Input.GetKeyDown(KeyCode.Space))
+
+        catch (Exception e)
         {
-            ws.Close();
-            Debug.Log("Websoket Closed");
+            Debug.LogError("Error parsing score data: " + e.Message);
         }
+    }
+
+    public void Update()
+    {
+        if (ws == null) return;
+    }
+
+    private void OnDestroy()
+    {
+        ws.Close();
+        ws = null;
+    }
+
+    [Serializable]
+    private class ScoreData
+    {
+        // public string id;
+        public int Counter;
     }
 
 
